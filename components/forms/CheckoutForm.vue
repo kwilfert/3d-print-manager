2637cebbce36992d6e154d82d2f909955162367f
kwilfert <kay.wilfert@gmail.com>
2024-02-19 16:11:05 +0100

    feat: draft of Checkout Form

---
+++ components/forms/CheckoutForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card title="Kasse">
+    Auflistung der Kosten
+    <ol>
+      <li>Katalog-Modell-kosten immer pauschal, Sonderdruck stets nach Größe und Filament</li>
+      <li>Menge der Figuren (Bulk bestellung? überhaupt erlauben?)</li>
+      <li>Druckkosten als Faktor von Größe und Filament/Resin</li>
+      <li>Versandkosten abhängig von Größe, Tracking soll immer dabei sein (oder 6,99EUR pauschal mit Tracking?)</li>
+      <li>Wichtig: "Kostenpflichtig bestellen" button</li>
+    </ol>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
