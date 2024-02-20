69acb398653137d42dbe4a48ecaaec62838e3b04
kwilfert <kay.wilfert@gmail.com>
2024-02-20 08:03:42 +0100

    feat: Management Panel Landing Page

---
+++ pages/management/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+const router = useRouter();
+
+const navigateTo = (path) => {
+  router.push(path)
+}
+// Add more functions for other sub-pages
+</script>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-card color="blue-lighten-1" prepend-icon="mdi-printer-3d" title="Modellverwaltung">
+          <v-card-text>
+            <v-btn text=">> Zur Modellverwaltung" variant="text" @click="navigateTo('/management/modelpanel')"/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card color="red" prepend-icon="mdi-atom-variant" title="Materialsortiment">
+          <v-card-text>
+            <v-btn text=">> Zur Materialverwaltung" variant="text" @click="navigateTo('/management/materialpanel')"/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card color="green" prepend-icon="mdi-image" title="Galerieverwaltung">
+          <v-card-text>
+            <v-btn text=">> Zur Galerieverwaltung" variant="text" @click="navigateTo('/management/gallerypanel')"/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
