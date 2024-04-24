c82ec30b3c5f6594260d5d418cedacffe8e021ba
kwilfert <kay.wilfert@gmail.com>
2024-04-24 15:04:05 +0200

    chore: add prod deployment script

---
+++ scripts/deploy_prod.js
@@ -0,0 +1,18 @@
+// upload.js
+import {exec} from 'child_process';
+import {server, username} from "../secrets.js";
+
+
+const command = `scp -r .output/* ${username}@${server}:/var/www/kaw-solutions/printservice`;
+
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`STDERR: ${stderr}`);
+        return;
+    }
+    console.log(`Upload successful: ${stdout}`);
+});
