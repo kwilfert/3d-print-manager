672432608383d2951225bd73cd0c0f02239bc678
kwilfert <kay.wilfert@gmail.com>
2024-05-10 09:24:28 +0200

    chore: add tailwind css
    - add tailwind package
    - add tailwind config/nuxt plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,13 @@ import {transformAssetUrls} from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    css: ['@/assets/css/main.css'],
     modules: [
         'vuetify-nuxt-module',
         '@formkit/auto-animate',
         '@pinia/nuxt',
         '@pinia-plugin-persistedstate/nuxt',
+        "@nuxtjs/tailwindcss"
     ],
     vite: {
         vue: {
