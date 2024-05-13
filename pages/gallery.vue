b370b4a970cf5c3aa7e2df6c32d7c68bfc90eea3
kwilfert <kay.wilfert@gmail.com>
2024-05-13 09:16:35 +0200

    feat: Instagram Placeholder Gallery
    - show specified accounts plainly posted images
    - no reels, no stories or whatever. sadly only the plain images

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-
+import BeholdWidget from "@behold/vue"
 </script>
-
 <template>
   <v-container max-width="1000">
     <div class="text-3xl font-bold text-center mt-10">Gallery</div>
-    <div class="text-xl mt-10 text-center">> Instagram Embed hier reinklatschen, bis eigene Implementierung stattfindet <</div>
+    <div class="text-xl mt-10 text-center">> Instagram Embed hier reinkgeklatscht, bis eigene Implementierung
+      stattfindet <
+    </div>
+    <behold-widget feed-id="nNYjbnBrtIqs8vz9d1kO"/>
   </v-container>
 </template>
 
