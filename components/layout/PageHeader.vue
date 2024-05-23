452f49bde2939fd750304420d06d06d33d56cc99
kwilfert <kay.wilfert@gmail.com>
2024-05-23 16:48:22 +0200

    feat: new banner and banner style
    - abstract header image
    - new css style
    - responsive header height
    - new nav bar style

--- components/layout/PageHeader.vue
+++ components/layout/PageHeader.vue
@@ -1,26 +1,36 @@
 <script lang="ts" setup>
-import deepmind from "@/assets/images/pexels-google-deepmind-17485708.jpg"
 import TopNavigation from "~/components/layout/TopNavigation.vue";
+import banner from "~/assets/images/3302177.jpg";
 </script>
 
 <template>
   <v-sheet>
     <!--Parallax Background and Page Title -->
-    <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="300">
-      <div class="inline-block align-middle ms-10 mt-20 text-white font-semibold align-middle">
-        <p class="text-4xl">
-          <span class="underline">KAW-Prints</span>: 3D-Druck mit Herz und Seele
-        </p>
-        <p class="mt-2 text-3xl">
-          Print-on-Demand-Service für Filament und UV-Harz
-        </p>
+    <v-parallax :src="banner" class="banner">
+      <div class="ms-5 mt-10">
+        <div class="ms-5 mt-10 text-white font-weight-bold">
+          <p class="text-h4">
+            <span class="text-decoration-underline">KAW-Prints</span><span class="d-none d-sm-block">3D-Druck mit Herz und Seele</span>
+          </p>
+          <p class="mt-2 text-h5 d-none d-sm-block">
+            Print-on-Demand-Service für Filament und UV-Harz
+          </p>
+        </div>
       </div>
+      <!--Nav bar with centered nav items in nuxt. Should stick to bottom of header -->
+      <TopNavigation class="position-absolute bottom-0"/>
     </v-parallax>
-    <!--Nav bar with centered nav items in nuxt. Should stick to bottom of header -->
-    <top-navigation/>
   </v-sheet>
 </template>
 
 <style scoped>
+.banner {
+  @media (max-width: 600px) {
+    height: 200px;
+  }
 
+  @media (min-width: 600px) {
+    height: 300px;
+  }
+}
 </style>
--- components/layout/TopNavigation.vue
+++ components/layout/TopNavigation.vue
@@ -9,17 +9,28 @@ const navigation = ref([
 </script>
 
 <template>
-  <v-container fluid class="border">
+  <v-container fluid>
     <v-row justify="center">
-      <v-col v-for="nav in navigation" :key="nav.label" class="text-center" cols="auto">
-        <div class="inline-block align-middle font-semibold ps-2 pe-2">
-          <nuxt-link :to="nav.path" class="font-bold">{{ nav.label }}</nuxt-link>
-        </div>
+      <v-col v-for="nav in navigation" :key="nav.label" align-self="center" class="text-center" cols="auto">
+        <nuxt-link :to="nav.path" active-class="text-decoration-underline"
+                   class="font-weight-bold text-white text-decoration-none">
+          <div class="align-center px-md-4 px-sm-2 px-1 py-2 rounded-lg bg-semi-transparent">
+            {{ nav.label }}
+
+          </div>
+        </nuxt-link>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <style scoped>
-
+.bg-semi-transparent {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+}
 </style>
