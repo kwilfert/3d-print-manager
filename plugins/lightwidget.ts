3d036d86dbc30d66d51f244bc25dbe925956dadc
kwilfert <kay.wilfert@gmail.com>
2024-05-24 18:24:23 +0200

    fix: wrap nuxt plugin lightwidget
    - wrap in defineNuxtPlugin for better interaction with nuxt env

--- plugins/lightwidget.ts
+++ plugins/lightwidget.ts
@@ -1,8 +1,8 @@
-export default () => {
+export default defineNuxtPlugin(() => {
     if (process.client) {
         const script = document.createElement('script');
         script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
         script.async = true;
         document.body.appendChild(script);
     }
-};
+});
